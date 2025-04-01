b05d4686b50a29a94d93cab9340dedd02da7adcd
Ben-MR <b.rieck@web.de>
2025-04-01 13:33:46 +0200

    added scripts for redner tasks

--- asset/templats/board_tasks-template.js
+++ asset/templats/board_tasks-template.js
@@ -1,86 +1,87 @@
-function showInProgressTasks(index) {
+function showInProgressTasks(index, subtasksClass) {
     return `
             <div class="tasks-content">
-                <span class="tasks-content-category">User Story</span>
+                <span class="tasks-content-${getCategoryClass(index)}">${allTasks[index].category}</span>
                 <span class="tasks-content-title">${allTasks[index].title}</span>
                 <span class="tasks-content-description">${allTasks[index].description}</span>
-                <div class="tasks-content-done">
-                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="2" style="--value: 1; --max: 1;"></progress>
-                    <span class="tasks-content-done-text">1/2 Subtasks</span>
-                </div>
+                
+                <div id="subtasks" class="tasks-content-done ${subtasksClass}">
+                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="${subTaskLenght(index)}" style="--value: 1; --max: ${subTaskLenght(index)};"></progress>
+                    <span class="tasks-content-done-text">1/${subTaskLenght(index)} Subtasks</span>
+                </div>                
                 <div class="assigned-priority-container">
                     <div class="assigned-container">
-                        <span class="assigned1">AM</span>
-                        <span class="assigned1">EM</span>
-                        <span class="assigned1">MB</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
                     </div>
-                    <div class="priority"></div>
+                    <div class="priority-${allTasks[index].priority}"></div>
                 </div>
             </div>
         `
 }
 
-function showToDoTasks(index) {
+function showToDoTasks(index, subtasksClass) {
     return `
             <div class="tasks-content">
-                <span class="tasks-content-category">User Story</span>
+                <span class="tasks-content-${getCategoryClass(index)}">${allTasks[index].category}</span>
                 <span class="tasks-content-title">${allTasks[index].title}</span>
                 <span class="tasks-content-description">${allTasks[index].description}</span>
-                <div class="tasks-content-done">
-                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="2" style="--value: 1; --max: 1;"></progress>
-                    <span class="tasks-content-done-text">1/2 Subtasks</span>
+                <div class="tasks-content-done ${subtasksClass}">
+                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="${subTaskLenght(index)}" style="--value: 1; --max: ${subTaskLenght(index)};"></progress>
+                    <span class="tasks-content-done-text">1/${subTaskLenght(index)} Subtasks</span>
                 </div>
                 <div class="assigned-priority-container">
                     <div class="assigned-container">
-                        <span class="assigned1">AM</span>
-                        <span class="assigned1">EM</span>
-                        <span class="assigned1">MB</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
                     </div>
-                    <div class="priority"></div>
+                    <div class="priority-${allTasks[index].priority}"></div>
                 </div>
             </div>
         `
 }
 
-function showAwaitFeedbackTasks(index) {
+function showAwaitFeedbackTasks(index, subtasksClass) {
     return `
             <div class="tasks-content">
-                <span class="tasks-content-category">User Story</span>
+                <span class="tasks-content-${getCategoryClass(index)}">${allTasks[index].category}</span>
                 <span class="tasks-content-title">${allTasks[index].title}</span>
                 <span class="tasks-content-description">${allTasks[index].description}</span>
-                <div class="tasks-content-done">
-                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="2" style="--value: 1; --max: 1;"></progress>
-                    <span class="tasks-content-done-text">1/2 Subtasks</span>
+                <div class="tasks-content-done ${subtasksClass}">
+                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="${subTaskLenght(index)}" style="--value: 1; --max: ${subTaskLenght(index)};"></progress>
+                    <span class="tasks-content-done-text">1/${subTaskLenght(index)} Subtasks</span>
                 </div>
                 <div class="assigned-priority-container">
                     <div class="assigned-container">
-                        <span class="assigned1">AM</span>
-                        <span class="assigned1">EM</span>
-                        <span class="assigned1">MB</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
                     </div>
-                    <div class="priority"></div>
+                    <div class="priority-${allTasks[index].priority}"></div>
                 </div>
             </div>
         `
 }
 
-function showDoneTasks(index) {
+function showDoneTasks(index, subtasksClass) {
     return `
             <div class="tasks-content">
-                <span class="tasks-content-category">User Story</span>
+                <span class="tasks-content-${getCategoryClass(index)}">${allTasks[index].category}</span>
                 <span class="tasks-content-title">${allTasks[index].title}</span>
                 <span class="tasks-content-description">${allTasks[index].description}</span>
-                <div class="tasks-content-done">
-                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="2" style="--value: 1; --max: 1;"></progress>
-                    <span class="tasks-content-done-text">1/2 Subtasks</span>
+                <div class="tasks-content-done ${subtasksClass}">
+                    <progress class="tasks-content-done-progressbar" class="progress-bar" value="1" max="${subTaskLenght(index)}" style="--value: 1; --max: ${subTaskLenght(index)};"></progress>
+                    <span class="tasks-content-done-text">1/${subTaskLenght(index)} Subtasks</span>
                 </div>
                 <div class="assigned-priority-container">
                     <div class="assigned-container">
-                        <span class="assigned1">AM</span>
-                        <span class="assigned1">EM</span>
-                        <span class="assigned1">MB</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
+                        <span class="assigned" style="background-color: ${randomBackgroundColor()};">AM</span>
                     </div>
-                    <div class="priority"></div>
+                    <div class="priority-${allTasks[index].priority}"></div>
                 </div>
             </div>
         `
--- js/board.js
+++ js/board.js
@@ -3,7 +3,10 @@ let allTasks = [];
 async function init() {
     let activeUser = JSON.parse(localStorage.getItem("activeUser"));
     renderInitials(activeUser);
-    await getAllTasks();    
+    await getAllTasks();  
+    await  renderTasks();  
+    randomBackgroundColor();
+    
 }
 
 function renderInitials(activeUser) {
@@ -22,24 +25,91 @@ async function getAllTasks() {
     let keys = Object.keys(responseJson);  
     for (let index = 0; index < keys.length; index++) {
         let task = responseJson[keys[index]];
-        allTasks.push(task); 
-         
-    }     
-    console.log(allTasks); 
-    renderProgressTasks();   
-    
+        allTasks.push(task);          
+    }          
 }
 
-
-function renderProgressTasks() {
-    let inProgress = document.getElementById('inProgress'); 
-    let ToDo = document.getElementById('ToDo'); 
-   
+async function renderTasks() {  
     for (let index = 0; index < allTasks.length; index++) {
-        if (allTasks[index].status === "in Progress") {
-            inProgress.innerHTML += showInProgressTasks(index);        
-        } 
-        if (allTasks[index].status === "To do") {
-            ToDo.innerHTML += showToDoTasks(index);        
-        } 
-    }}
+        renderProgress(index);
+        renderTodo(index);
+        renderAwaitFeedback(index);
+        renderDone(index);    
+        
+    }
+}
+
+function renderProgress(index) {
+    let inProgress = document.getElementById('inProgress'); 
+    let subtasksClass = (!allTasks[index].subtasks) ? 'd-none' : '';
+    if (allTasks[index].status === "in Progress") {
+        inProgress.innerHTML += showInProgressTasks(index, subtasksClass);  // Übergabe von categoryColor an showInProgressTasks
+    } 
+}
+
+function renderTodo(index) {
+    let ToDo = document.getElementById('ToDo'); 
+    let subtasksClass = (!allTasks[index].subtasks) ? 'd-none' : '';
+    if (allTasks[index].status === "To do") {
+        ToDo.innerHTML += showToDoTasks(index, subtasksClass);        
+    } 
+}
+
+function renderAwaitFeedback(index) {
+    let await = document.getElementById('await');
+    let subtasksClass = (!allTasks[index].subtasks) ? 'd-none' : '';
+    if (allTasks[index].status === "await Feedback") {
+        await.innerHTML += showAwaitFeedbackTasks(index, subtasksClass);        
+    } 
+}
+
+function renderDone(index) {
+    let done = document.getElementById('done'); 
+    let subtasksClass = (!allTasks[index].subtasks) ? 'd-none' : '';
+    if (allTasks[index].status === "done") {
+        done.innerHTML += showDoneTasks(index, subtasksClass);        
+    }           
+}
+
+function getCategoryClass(index) {
+    let category = allTasks[index].category;
+    let categoryName = category.split(' ')[0];  
+    return categoryName.toLowerCase(); 
+}
+
+function subTaskLenght(index) {
+    if (allTasks[index].subtasks) {
+        subTaskLength = allTasks[index].subtasks.length
+    } else {
+    subTaskLength = ""
+    }
+    return subTaskLength;
+}
+
+function randomBackgroundColor(index) {
+    const Colors = [
+        "#3498db", // bright blue
+  "#9b59b6", // purple
+  "#e74c3c", // red
+  "#f39c12", // orange
+  "#1abc9c", // turquoise
+  "#2ecc71", // green
+  "#34495e", // dark grey
+  "#16a085", // dark teal
+  "#2980b9", // ocean blue
+  "#8e44ad", // dark purple
+  "#d35400", // orange red
+  "#c0392b", // brick red
+  "#27ae60", // emerald green
+  "#f1c40f", // yellow
+  "#f66d44", // coral orange
+  "#e67e22", // carrot orange
+  "#7f8c8d", // grey
+  "#95a5a6", // light grey
+  "#dcdde1", // light slate grey
+  "#9b59b6"  // lavender purple
+      ];
+    let backgroundColor = Colors[Math.floor(Math.random()*Colors.length)];
+    console.log(backgroundColor);
+    return backgroundColor;    
+}
