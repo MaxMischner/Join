bbe9c052bd7e8f19d8959a6abb645ffbb1b333a1
Ben-MR <b.rieck@web.de>
2025-04-09 11:58:57 +0200

    added wrong email and password to login

--- js/log_in.js
+++ js/log_in.js
@@ -1,12 +1,12 @@
 let activeUser = [];
 
-async function init() {
-    let response = await fetch(BASE_URL_USER + ".json");
-    let responseJSON = await response.json();
-    console.log(responseJSON);    
-    let keys = Object.keys(responseJSON);
-    console.log(keys);    
-}
+// async function init() {
+//     let response = await fetch(BASE_URL_USER + ".json");
+//     let responseJSON = await response.json();
+//     console.log(responseJSON);    
+//     let keys = Object.keys(responseJSON);
+//     console.log(keys);    
+// }
 
 async function login() {
     let email = document.getElementById('loginEmail').value;
@@ -18,11 +18,20 @@ async function login() {
     if(user) {
         activeUser.push (user);
         localStorage.setItem("activeUser", JSON.stringify(activeUser));
-        window.location.href = "summary.html";
- 
+        window.location.href = "summary.html"; 
     }else {
-        console.log('nicht gefunden');        
+        wrongLogin();
     }    
+}
+
+function wrongLogin() {
+    const existingWarning = document.getElementById('loginError');
+    if (existingWarning) return;
+    let newElement = document.createElement('div');       
+    newElement.textContent = 'Email or password incorrect';
+    newElement.classList.add("wrong-login-text");
+    newElement.id = 'loginError';
+    document.getElementById('loginForm').appendChild(newElement); 
 }
 
 function guestLogin() {
