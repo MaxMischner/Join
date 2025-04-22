82af1ec2a5be8157c52121dd973fbfc12d101b10
Ben-MR <b.rieck@web.de>
2025-04-22 09:56:28 +0200

    bugfix board

--- js/board.js
+++ js/board.js
@@ -92,9 +92,9 @@ function getBackgroundColorNames(name) {
     let contact = allContactsBoard.find(c =>
         c.name.trim().replace(/\u200B/g, '').toLowerCase() === nameToFind
     );
-
-    return contact ? contact.color : '#ccc'; // fallback color oder null, wie du willst
+    return contact ? contact.color : 'null'; 
 }
+
 /**
  * Renders the data from allTasks in the the four splits of the board: To-Do, Progress, Await Feedback and Done
  * 
